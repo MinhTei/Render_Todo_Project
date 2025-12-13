1b43e006177538b1d9f0f4e6eafb5e8582ca85e1
Minh Tai <buiminhtai97@example.com>
2025-12-14 00:50:01 +0700

    tesst

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = process.env.PORT || 3000 // Render sẽ tự điền cổng vào đ
 
 app.get('/', (req, res) => {
   res.send('<h1>Chao mung den voi Cloud Platform (Render) 🚀</h1><p>Trang web nay duoc Deploy tu dong!</p>')
+     res.send('<p>Ban co the tham khao them ve Render tai <a href="https://render.com/docs">Render Documentation</a></p>')
 })
 
 app.listen(port, () => {
